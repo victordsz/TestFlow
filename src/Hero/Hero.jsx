7ce2dd31a37f345035f5024c72a4a0fdb5e8b407
Bianca-Caetano <biancacaetano079@gmail.com>
2026-02-07 10:35:34 -0300

    :sparkles: Adicionando uma nova branch e uma nova pasta Hero

---
+++ src/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import App from "./App.jsx";
+export default function Hero () {
+    return (
+        <div className="hero">
+            <h2>Bom dia. Reunião com Israel e Victor</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, 
+                cum, quaerat, veniam ipsa at esse temporibus repudiandae fugiat reiciendis 
+                sunt vel obcaecati amet eius impedit accusamus error deleniti odit ut.
+            </p>    
+        </div>
+
+    );
+}
